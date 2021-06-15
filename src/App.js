d17d78b46b2bf97874d80be0e6f061c54adf2eea
Andrew Jenkins <jenkins_aj@yahoo.com>
2021-06-15 11:06:24 +0100

    more practice with async useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <h1>Hello World</h1>
+  const [subscribed, setSubscribed] = useState(false);
+  const [subscribedCount, setSubscribedCount] = useState(0);
+  const [alerts, setAlerts] = useState(false);
+
+  return (
+    <section>
+      <p>Please click to subscribe to my updates!</p>
+      <p>Subscriber Count: {subscribedCount}</p>
+      <button
+        onClick={() => {
+          setSubscribed(!subscribed);
+          //setSubscribedCount(subscribedCount + 1); //even with these two lines, the subscription count would only
+          //setSubscribedCount(subscribedCount + 1); //update by one because useState is async
+          setSubscribedCount((currentCount) => subscribed ? currentCount - 1 : currentCount + 1);
+          setSubscribedCount((currentCount) => subscribed ? currentCount - 1 : currentCount + 1);
+          if (!alerts) setAlerts(true);
+        }}
+      >
+        {subscribed ? "Unsubscribe" : "Subscribe"}
+      </button>
+      <button onClick={() => setAlerts(!alerts)}>
+        {alerts ? "Stop Email Alerts" : "Get Email Alerts"}
+      </button>
+    </section>
+  );
 }
 
 export default App;
